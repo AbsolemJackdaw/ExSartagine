8b1a18e9a19da7474cb44484e084f499ee0eefff
ArtixAllMighty <robbeenaxel@hotmail.com>
2018-01-21 08:15:30 +0100

    versioning

--- src/main/java/subaraki/exsartagine/util/Reference.java
+++ src/main/java/subaraki/exsartagine/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MODID = "exsartagine";
     public static final String NAME = "Ex Sartagine mod";
-    public static final String VERSION = "1.12.2 0.1.0.0";
+    public static final String VERSION = "${version}";
     public static final String DEPENDENCY = "required-after:subcommonlib";
     public static final String CLIENT_PROXY = "subaraki.exsartagine.proxy.ClientProxy";
     public static final String SERVER_PROXY = "subaraki.exsartagine.proxy.ServerProxy";
